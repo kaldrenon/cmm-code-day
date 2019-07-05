07d7e125380f98974ac7a559ce061086d04c39a5
Andrew Fallows <andrew.fallows@intervarsity.org>
2019-07-05 15:35:32 -0400

    init user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/login'
+  get 'users/register'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "application#index"
+  root to: "users#login"
 end
