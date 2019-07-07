2bfb8717eec95658fd6b56f5abca32e1b3b53e4b
Andrew Fallows <andrew.fallows@intervarsity.org>
2019-07-07 12:22:03 -0400

    item specs

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'create' do
+    it 'can be initialized' do
+      expect(Item.new).to be_a_new(Item)
+    end
+
+    it 'can be saved empty' do
+      expect(Item.create).not_to be_a_new(Item)
+
+      expect {
+        Item.create
+      }.to change(Item, :count).by(1)
+    end
+  end
 end
