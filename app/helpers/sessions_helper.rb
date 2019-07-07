d9efab45f1293e72edfbfa2664fe1e31851f93e4
Andrew Fallows <andrew.fallows@intervarsity.org>
2019-07-07 11:51:11 -0400

    spec sessions_helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,13 +5,13 @@ module SessionsHelper
 
   def current_user
     if session[:user_id]
-      @current_user ||= User.find(id: session[:user_id])
+      @current_user ||= User.find(session[:user_id])
     else
       nil
     end
   end
 
   def logged_in?
-    current_user
+    !!current_user
   end
 end
--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -11,5 +11,39 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe SessionsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    session.delete(:user_id)
+    @user = create(:user)
+  end
+
+  describe 'log_in' do
+
+    it 'updates the session with user data' do
+      log_in(@user)
+
+      expect(session[:user_id]).to eq @user.id
+    end
+  end
+
+  describe 'current_user' do
+    it 'returns nil if no user logged in' do
+      expect(current_user).to eq nil
+    end
+
+    it 'returns the user if logged in' do
+      log_in(@user)
+      expect(current_user).to eq @user
+    end
+  end
+
+  describe 'current_user' do
+    it 'returns nil if no user logged in' do
+      expect(logged_in?).to eq false
+    end
+
+    it 'returns the user if logged in' do
+      log_in(@user)
+      expect(logged_in?).to eq true
+    end
+  end
 end
